9c426b435adb5cfbfa4804fb0e1a5afcbabb5dd7
abdurrahman rajab <rajab-2@windowslive.com>
2020-06-06 00:46:33 +0300

    Add workshop button
    Show the button automatically on time. 
    Format the script

--- introToJS/script.js
+++ introToJS/script.js
@@ -2,15 +2,21 @@ const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
+let showTime;
 
-let countDown = new Date(2021, 06, 06, 18).getTime(),
-    x = setInterval(function() {    
+let countDown = new Date(2020, 05, 06, 18).getTime(),
+  x = setInterval(function () {
 
-      let now = new Date().getTime(),
-          distance = countDown - now;
+    let now = new Date().getTime(),
+      distance = countDown - now;
+      showTime = new Date(2020, 05, 06, 17, 40).getTime() - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-    }, second)
+    if (showTime <= 0) {
+      document.getElementById('workshopLink').hidden = false;
+    }
+    document.getElementById('days').innerText = Math.floor(distance / (day)),
+      document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+  }, second)
+
